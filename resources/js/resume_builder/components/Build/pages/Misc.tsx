d1fe0af66993e831953abc929991afd3cfbf8b9e
Ali Raza <raza_997@hotmail.com>
2023-04-28 16:00:21 +0500

    updated

--- resources/js/resume_builder/components/Build/pages/Misc.tsx
+++ resources/js/resume_builder/components/Build/pages/Misc.tsx
@@ -23,15 +23,18 @@ export const AddURLOrWebsite = () => {
 
             <div className="flex flex-row w-full justify-between">
                 <div className="flex flex-row w-full gap-3">
-                    <select className="shadow px-3 text-blue-400 text-md font-bold">
-                        {URLArray.map((v,k) => 
-                            <option className="text-blue-400" value={v}>{v}</option>
+                    <div className="flex flex-row shadow gap-3 w-3/4">
 
-                       ) }
-                    </select>
+                        <select className="px-3 text-blue-400 text-md font-bold outline-none">
+                            {URLArray.map((v,k) => 
+                                <option className="text-md font-bold  text-gray-600" value={v}>{v}</option>
+
+                        ) }
+                        </select>
                     
-                    <input className="shadow h-8  w-full text-blue-500 outline-none border-b"></input>
-                    <div className="flex flex-row shadow justify-center gap-3 px-3">
+                        <input className="h-8 w-2/3 text-blue-500 outline-none border-b overflow-y-auto"></input>
+                        </div>
+                    <div className="flex flex-row shadow justify-center gap-3 px-3 w-1/4">
 
                         <button className=" text-red-600"><i className="fa fa-minus"></i></button>
                         <button className=" text-blue-900"><i className="fa fa-plus"></i></button>
