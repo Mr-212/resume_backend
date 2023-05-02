79e69a5c41c189670a5e9ef756ab64b7893e4123
Ali Raza <raza_997@hotmail.com>
2023-05-03 00:41:19 +0500

    updated

--- resources/js/resume_builder/components/Build/pages/Misc.tsx
+++ resources/js/resume_builder/components/Build/pages/Misc.tsx
@@ -14,54 +14,93 @@ const URLArray = [
     'Twitter',
 ];
 
+
+const social = { name: 'Website', value: "https::/google.com" }
+
 export const AddURLOrWebsite = () => {
 
     const [values, setValue] = useState([]);
 
 
     const AddNewUrl = () => {
-
+        setValue([...values, social]);
     };
 
     useEffect(() => {
 
-    },[]);
+    }, []);
 
 
+    const removeUrl = () => {
 
+    }
 
 
     return(
         <>
-
             <div className="flex flex-col w-full justify-between">
                 <div>
-                    <button className="py-1.5 px-4 bg-gray-600 text-blue-400 shadow">Add Sociala/Website</button>
+                    <button className="py-1.5 px-4 bg-gray-600 text-blue-400 shadow" onClick={() => AddNewUrl()}>Add Social/Website</button>
                 </div>
-                <div className="flex flex-row w-full gap-3">
-                    <div className="flex flex-row shadow gap-3 w-3/4">
+                <div className="flex flex-col w-full gap-2">
 
-                        <select className="px-3 text-blue-400 text-md font-bold outline-none">
-                            {URLArray.map((v,k) => 
-                                <option className="text-md font-bold  text-gray-600" value={v}>{v}</option>
+                { values.map((val, key) => 
+                //     <div className="flex flex-row w-full py-2" key={key}>
+                //     <div className="flex flex-row gap-3 w-3/4">
 
-                        ) }
-                        </select>
+                //         <select className="px-3 text-blue-400 text-md font-bold outline-none">
+                //             {URLArray.map((v,k) => 
+                //                 <option className="text-md font-bold  text-gray-600" value={val['name']}>{v}</option>
+
+                //             )}
+                //         </select>
                     
-                        <input className="h-8 w-2/3 text-blue-500 outline-none border-b overflow-y-auto"></input>
-                        </div>
-                    <div className="flex flex-row shadow justify-center gap-3 px-3 w-1/4">
+                //         <input className="pl-4 h-8 w-2/3 text-blue-500 outline-none border-b overflow-y-auto" value={val['value']}></input>
 
-                        <button className=" text-red-600"><i className="fa fa-minus"></i></button>
-                        {/* <button className=" text-blue-900"><i className="fa fa-plus"></i></button> */}
-                    </div> 
+                //     </div>
+                //     <div className="flex flex-row  justify-center gap-3 px-3 w-1/4">
 
-                </div>
-                 
-                  
+                //         <button className="text-red-600"><i className="fa fa-minus"></i></button>
+                //     </div> 
+
+                // </div>
+                <AddURLOrWebsiteComponent data={val} key={key}></AddURLOrWebsiteComponent>
+                )
+            }
+            </div>
+
+                            
             </div>
             
         </>
+    )
+}
+
+
+const AddURLOrWebsiteComponent = ({data}) => {
+    // console.log(data)
+
+    const [value, setValue] = useState(data.value);
+
+    return(
+        <div className="flex flex-row w-full py-2" key={data.name}>
+        <div className="flex flex-row gap-3 w-3/4">
+
+            <select className="px-3 text-blue-400 text-md font-bold outline-none">
+                {URLArray.map((v,k) => 
+                    <option className="text-md font-bold  text-gray-600" value={data.name}>{v}</option>
+                )}
+            </select>
+        
+            <input className="pl-4 h-8 w-2/3 text-blue-500 outline-none border-b overflow-y-auto" value={value} onChange={e=>setValue(e.currentTarget.value)}></input>
+
+        </div>
+        <div className="flex flex-row  justify-center gap-3 px-3 w-1/4">
+
+            <button className="text-red-600"><i className="fa fa-minus"></i></button>
+        </div> 
+
+    </div>
     )
 }
 
