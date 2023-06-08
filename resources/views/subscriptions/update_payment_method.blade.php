a5e633e6b3ed89b9fd1dc6cf718a26005865dd66
Ali Raza <raza_997@hotmail.com>
2023-06-08 19:57:47 +0500

    updated .

--- resources/views/subscriptions/update_payment_method.blade.php
+++ resources/views/subscriptions/update_payment_method.blade.php
@@ -6,21 +6,21 @@
         <div class="col-md-6">
 
         </div>    
-        <div class="col-md-4 border shadow bg-gray-50 p-3 space-y-5 float-right">
+        <div class="col-md-4 border shadow bg-gray-50 px-10 py-5 float-right">
             <div class="border-b-2 border-blue-400 py-2">
                 <h4 class="font-bold text-md">Payment Method Info</h4>
             </div>
-            <div class="space-y-2">
 
-                <input class="form-control form-control-sm outline-none" id="card-holder-name" placeholder="Card Holder Name" type="text">
+            <form class="space-y-3" id="payment_form" action="/subscriptions/create" method="POST">
+                <input class="w-full text-sm  border-b-2 outline-none" id="card-holder-name" placeholder="Card Holder Name" type="text">
     
                 <!-- Stripe Elements Placeholder -->
-                <div class="form-control form-control-sm" id="card-element"></div>
+                <div class="w-full border-b-2" id="card-element"></div>
                 
                 <button type="button" class="px-2 py-1 bg-blue-400 text-gray-50  hover:bg-black hover:text-white rounded-none float-right font-bold" id="card-button" data-secret="{{ $intent->client_secret }}">
                     Update Payment Method
                 </button>
-            </div>
+            </form>
            
         </div>
 
@@ -35,14 +35,10 @@
 
     console.log("{{env('STRIPE_KEY')}}")
     const stripe = Stripe("pk_test_51L2wxGDoh4KpmE6IrbjsAB1Lig622SuWSoQBrljuhf95bxtEq2jdpnIVJDDYfkoPxmzvdgXYSRdwRWySQyW8JiBj00HSGY1FSy");
- 
     const elements = stripe.elements();
-    const cardElement = elements.create('card');
- 
+    const cardElement = elements.create('card',{ hidePostalCode: true });
     cardElement.mount('#card-element');
-</script>
 
-<script>
     const cardHolderName = document.getElementById('card-holder-name');
     const cardButton = document.getElementById('card-button');
     const clientSecret = cardButton.dataset.secret;
@@ -60,7 +56,9 @@
         if (error) {
             // Display "error.message" to the user...
         } else {
-            // The card has been verified successfully...
+            //console.log("The card has been verified successfully...");
+            document.getElementById('payment_form').submit();
+
         }
     });
 </script>
