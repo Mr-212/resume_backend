74847358a39297fdec6ee3b1f65271654bf8c859
Ali Raza <raza_997@hotmail.com>
2023-07-22 13:15:33 +0500

    cors file

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,14 +39,14 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\HandleInertiaRequests::class,
-            \Fruitcake\Cors\HandleCors::class,
+            // \Fruitcake\Cors\HandleCors::class,
         ],
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \Fruitcake\Cors\HandleCors::class,
+            // \Fruitcake\Cors\HandleCors::class,
         ],
     ];
 
