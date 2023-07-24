d721675316949f6627a0b9e90f41e7d5a3f281e8
Ali Raza <raza_997@hotmail.com>
2023-07-24 22:00:23 +0500

    upated

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -6,13 +6,17 @@
         </div>
         <div class="row">
 
-            <div class="col-md-3">
+            <div class="col-md-4">
                 
             </div>
-            <div class="col-md-6">
+
+            <div class="col-md-8">
+                <div>
+                    
+                </div>
                 
             </div>
-            <div class="col-md-3"></div>
+            {{-- <div class="col-md-3"></div> --}}
     </div>
     </div>    
 @endsection
