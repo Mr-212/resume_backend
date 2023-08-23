1a22fed37b42149e8de2a212812b9911b5e1df7a
Ali Raza <raza_997@hotmail.com>
2023-08-23 12:01:37 +0500

    updated

--- resources/views/layouts/partials/headersJs.blade.php
+++ resources/views/layouts/partials/headersJs.blade.php
@@ -2,5 +2,4 @@
         @include('layouts.partials.head')
         @vite('resources/css/app.css')
         @vite('resources/js/app.js')
-        <!-- @viteReactRefresh -->
         @vite('resources/js/app.jsx')
