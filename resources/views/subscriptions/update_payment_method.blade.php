9e9875479ef2e80310f3bc243e836f10263e435b
Ali Raza <raza_997@hotmail.com>
2024-07-31 00:16:25 +0500

    updated.

--- resources/views/subscriptions/update_payment_method.blade.php
+++ resources/views/subscriptions/update_payment_method.blade.php
@@ -17,47 +17,70 @@
             </div>
 
         </div>
-        @if(!empty($paymentMethods))
+
         <div class="col-md-6">
-            <div class="p-4 card shadow bg-gray-50 h-96">
-                <div class="header border-b-2 border-gray-400 py-2">
-                    <h4 class="text-center font-bold text-md py-2 text-black">Select Payament Method</h4>
-                    </div>
-                <div class="card-body">
-                    @foreach ($paymentMethods as $paymentMethod )
-                    <div class="flex flex-row justify-start space-x-2 shadow-lg">
-                        <input class="" type="radio" name="paymentMethod" value="{{ $paymentMethod->id}}" />
-                        <div class="flex flex-row justify-start space-x-2 w-full text-black font-bold">
-                            <span class="">{{ $paymentMethod->card->brand }}</span>
-                            <span class="">{{ $paymentMethod->card->last4 }}</span>
-                            <span class="">{{ $paymentMethod->card->exp_month }} / {{ $paymentMethod->card->exp_year }}</span>
+            @if(!empty($paymentMethods))
 
-                        </div>
-                    </div>
-
-                    @endforeach
-                </div>
-            </div>
             @endif
 
             <div class="p-5 card shadow bg-gray-50 h-96">
                 <div class="header border-b-2 border-gray-400 py-2">
-                    <h4 class="text-center font-bold text-md py-2 text-black">Add Payment Method Info</h4>
+                    <h4 class="text-center font-bold text-md py-2 text-black">Payment Method Info</h4>
                 </div>
+
 
                 <form class="card-body space-y-3" id="payment_form" action="/subscription" method="post">
                     @csrf
                     <input type="hidden" name="payment_method_id" id='payment_method_id' />
                     <input type="hidden" name="plan_id" value="{{$plan_id}}" />
+                    <div>
+                        <div>
 
-                    <input class="w-full text-sm  border-b-2 outline-none" id="card-holder-name" placeholder="Card Holder Name" type="text">
+                            <input type="radio" name="radio_payment_method" id="select_existing_card" class="radio_payment_method" value="existing"/>
+                            <label for="select_existing">Select Existing Payment Method</label>
+                        </div>
 
-                    <!-- Stripe Elements Placeholder -->
-                    <div class="py-1" id="card-element">
+                        <div>
+                            <input type="radio" name="radio_payment_method" id="add_new_card" class="radio_payment_method" value="new" />
+                            <label for="add_new_card">Add New Card</label>
+                        </div>
+                    </div>
+
+
+                    <div class="select_card mb-2">
+
+                        <select class="form-control border-none bg-gray-400 rounded-none" id="select_payment_method_id" placeholder="">
+                            @foreach ($paymentMethods as $paymentMethod )
+                                <option value="{{ $paymentMethod->id }}">
+                                    <div class="flex flex-row justify-start space-x-2 shadow-lg">
+                                        <div class="flex flex-row justify-start space-x-2 w-full text-black font-bold">
+                                            <span class="">{{ $paymentMethod->card->brand }}</span>
+                                            <span class="">{{ $paymentMethod->card->last4 }}</span>
+                                            <span class="">{{ $paymentMethod->card->exp_month }} / {{ $paymentMethod->card->exp_year }}</span>
+                                        </div>
+                                    </div>
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <div class="add_card">
+                        {{-- <input type="radio" name="payment_methood" /> --}}
+
+
+                        <h4 class="text-center font-bold text-md py-2 text-black">Add Payment Method</h4>
+
+                        <input class="w-full text-sm  border-b-2 outline-none" id="card-holder-name" placeholder="Card Holder Name" type="text">
+
+                        <!-- Stripe Elements Placeholder -->
+                        <div class="py-1" id="card-element">
+                        </div>
+                    </div>
+
 
                     </div>
                     <button type="button" class="w-full py-2 bg-blue-900 text-gray-50 hover:bg-black hover:text-white rounded-none float-right font-bold" id="card-button" data-secret="{{ $intent->client_secret }}">
-                        Subscribe
+                        Checkout
                     </button>
 
                 </form>
@@ -72,6 +95,15 @@
 
 @push('scripts')
 <script src="https://js.stripe.com/v3/"></script>
+
+<script>
+
+    $(document).ready(function() {
+        var is_checked = $('.radio_payment_method').val();
+        console.log(is_checked);
+
+    });
+</script>
 
 <script>
 
